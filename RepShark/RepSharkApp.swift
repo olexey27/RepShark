117008752a480d38d93aded33bbd24f40cb55976
olexey27 <allxyog@gmail.com>
2025-07-14 11:00:33 +0100

    Initial Commit

---
+++ RepShark/RepSharkApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepSharkApp.swift
+//  RepShark
+//
+//  Created by Alexej K on 14/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct RepSharkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
